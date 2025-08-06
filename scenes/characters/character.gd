9355cded0e7cf03ca204f3824ed975507c2253a6
th3gh0s8 <chamudithapasindu54@gmail.com>
2025-08-06 11:33:42 +0530

    player movement  implimented
    Co-Authored-By: SLRAPTOR <63906862+slraptor@users.noreply.github.com>

--- scenes/characters/character.gd
+++ scenes/characters/character.gd
@@ -4,6 +4,17 @@ extends CharacterBody2D
 @export var damage : int
 @export var speed : float
 
+@onready var animation_player := $AnimationPlayer 
+@onready var character_sprite := $CharacterSprite
+
+enum State {IDLE, WALK}
+
+var state = State.IDLE
+
+
+#func _ready() -> void:
+#	animation_player = get_node("AnimationPlayer")
+
 func _process(delta: float) -> void:
 	
 	#if Input.is_action_pressed("ui_right"):
@@ -17,8 +28,33 @@ func _process(delta: float) -> void:
 
 	#if Input.is_action_pressed("ui_down"):
 	#	position += Vector2.DOWN * delta * speed
-
-	var derection := Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	
 	#position += derection * delta * speed
-	velocity = derection * speed
+	handle_input()
+	hanle_movement()
+	handle_animations()
+	flip_sprites()
 	move_and_slide()
+	
+func hanle_movement():
+	if velocity.length() == 0:
+		state = State.IDLE
+	else :
+		state = State.WALK
+
+func handle_input() -> void:
+		var derection := Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+		velocity = derection * speed
+		
+func handle_animations() -> void:
+	if state == State.IDLE:
+		animation_player.play("idle")
+	elif state == State.WALK:
+		animation_player.play("walk")
+
+
+func flip_sprites() -> void:
+	if velocity.x > 0:
+		character_sprite.flip_h = false
+	elif velocity.x < 0:
+		character_sprite.flip_h = true
